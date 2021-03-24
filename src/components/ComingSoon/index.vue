bc57f5e39f4d7afc98b1ae97436f2150e3a80c70
hjt-sss <2243445753@qq.com>
2021-03-24 18:02:36 +0800

    @详情组件添加@hanjt@3.24

--- src/components/ComingSoon/index.vue
+++ src/components/ComingSoon/index.vue
@@ -4,9 +4,9 @@
     <Scroller v-else>
       <ul>
         <li v-for="item in movieList" :key="item.id">
-          <div class="pic_show"><img :src="item.img | setWH('128.180')"></div>
+          <div class="pic_show" @tap="handleToDetail(item.id)"><img :src="item.img | setWH('128.180')"></div>
           <div class="info_list">
-            <h2>{{item.nm}} <img v-if="item.version" src="@/assets/maxs.png" alt=""></h2>
+            <h2 @tap="handleToDetail(item.id)">{{item.nm}} <img v-if="item.version" src="@/assets/maxs.png" alt=""></h2>
             <p><span class="person">{{item.wish}}</span> 人想看</p>
             <p>主演: {{item.star}}</p>
             <p>{{item.rt}} 上映</p>
@@ -34,6 +34,9 @@ export default {
   },
 
   methods: {
+    handleToDetail(val){
+      this.$router.push('/movie/detail/2/'+val)
+    },
     queryMovieList(){
       let cityId = localStorage.getItem('nowCityId')
       if (this.prevCityId === cityId) { return; }
--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -5,9 +5,9 @@
       <ul>
         <li class="pullDown">{{pullDown}}</li>
         <li v-for="item in movieList" :key="item.id">
-          <div class="pic_show" @tap="handleToDetail"><img :src="item.img | setWH('128.180')"></div>
+          <div class="pic_show" @tap="handleToDetail(item.id)"><img :src="item.img | setWH('128.180')"></div>
           <div class="info_list">
-            <h2>{{item.nm}} <img v-if="item.version" src="@/assets/maxs.png" alt=""></h2>
+            <h2 @tap="handleToDetail(item.id)">{{item.nm}} <img v-if="item.version" src="@/assets/maxs.png" alt=""></h2>
             <p>观众评分 <span class="grade">{{item.sc}}</span></p>
             <p>主演: {{item.star}}</p>
             <p>{{item.showInfo}}</p>
@@ -44,8 +44,8 @@ export default {
     })
   },
   methods: {
-    handleToDetail(){
-      console.log('handle')
+    handleToDetail(val){
+      this.$router.push('/movie/detail/1/'+val)
     },
     handleToScroll(pos){
       if (pos.y > 30) {
